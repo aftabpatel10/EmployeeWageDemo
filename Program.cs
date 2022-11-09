4515d807f207eef36c41ca165fc7a008f993f8fb
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-09 20:34:27 +0530

    Modifying EmployeeAttendanceUC1 to CalculatingDailyWageUC2

--- Program.cs
+++ Program.cs
@@ -12,15 +12,28 @@ namespace EmployeeWageDemo
         {
             Console.WriteLine("Welcome to EmployeeWage Conmputation");
             //UC1
+            const int EMP_RATE_PER_HR = 20;
             int employeePresent = 1;
+            int empWage = 0;
+            int empHrs = 0;
+
             Random r = new Random();
             int empployeeIinput = r.Next(0, 2);
 
             if (employeePresent == employeeInput)
+            {
                 Console.WriteLine("Employee is Present");
+                empHrs = 8;
+
+            }
+                 
             else
+            {
+                //empHrs = 0;
                 Console.WriteLine("Employee is Absent");
-           
+            }
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Daily Employee Wage is:"+empWage); 
             Console.ReadLine();
         }
     }
