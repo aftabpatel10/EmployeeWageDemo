bc78449171d07fd61aaea0dbfa8abaf0ee169ea0
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-09 19:18:43 +0530

    Adding Welcome message into main Branch

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to EmployeeWage Conmputation");
+            Console.ReadLine();
+        }
+    }
+}
